de84fdedb78ee3c2b2d3c26b3798f7b95b095587
Waqas Ahmad <118092990+waqasahmad713@users.noreply.github.com>
2024-07-01 23:36:12 +0500

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Pass by Reference and Value/pass-by-reference-and-value.py
@@ -0,0 +1,23 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+
+class Solution:
+    def passedBy(self, a, b):
+        a+=1
+        b+=2
+        return a,b
+
+#{ 
+ # Driver Code Starts.
+if __name__ == '__main__': 
+    t = int(input ())
+    for _ in range (t):
+        a, b = list(map(int, input().split()))
+        ob = Solution()
+        res = ob.passedBy(a, b)
+        print(res[0], res[1])
+# } Driver Code Ends
