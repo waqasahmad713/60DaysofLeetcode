e2d58e846f5eb9e7e6ca5b2a44718f4d5c520853
Waqas Ahmad <118092990+waqasahmad713@users.noreply.github.com>
2024-07-07 18:00:12 +0500

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Pattern 7/pattern-7.py
@@ -0,0 +1,24 @@
+#User function Template for python3
+
+class Solution:
+    def printTriangle(self, N):
+        for i in range(N):
+            for j in range(N-i-1):
+                print(" ",end="")
+            for j in range(2*i+1):
+                print("*",end="")
+            print("")
+        
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        ob = Solution()
+        ob.printTriangle(N)
+# } Driver Code Ends
